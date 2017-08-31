ff8885d5302814ec6e131c41cbc5c355730e72f2
Alex Rukhlin <arukhlin@microsoft.com>
2017-08-31 18:15:20 -0400

    5th try

--- source/com.microsoft.tfs.core/src/com/microsoft/tfs/core/pendingcheckin/events/AffectedTeamProjectsChangedEvent.java
+++ source/com.microsoft.tfs.core/src/com/microsoft/tfs/core/pendingcheckin/events/AffectedTeamProjectsChangedEvent.java
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See License.txt in the repository root.
 
 
+
+
 package com.microsoft.tfs.core.pendingcheckin.events;
 
 import com.microsoft.tfs.core.clients.CoreClientEvent;
