9dd8d491c74ef64b56d8158fe52c60355f97076d
Alex Rukhlin <arukhlin@microsoft.com>
2017-08-31 10:16:52 -0400

    Concurrent change

--- source/com.microsoft.tfs.core/src/com/microsoft/tfs/core/pendingcheckin/events/AffectedTeamProjectsChangedListener.java
+++ source/com.microsoft.tfs.core/src/com/microsoft/tfs/core/pendingcheckin/events/AffectedTeamProjectsChangedListener.java
@@ -3,6 +3,10 @@
 
 package com.microsoft.tfs.core.pendingcheckin.events;
 
+
+/*
+
+ */
 import java.util.EventListener;
 
 import com.microsoft.tfs.core.pendingcheckin.PendingCheckinPendingChanges;
